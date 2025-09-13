7c2d0bdcb9dc95a85a075b259d26353ff0bf2bfd
Kammari Vishal <vishalkammari2102@gmail.com>
2025-09-13 20:35:08 +0530

    Time: 4 ms (37.56%), Space: 43.5 MB (35.82%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+
+
+    public static List<List<Integer>> function(int [] arr,int target,int n, int i,List<Integer> li,List<List<Integer>> result){
+       
+            if(target==0){
+
+            result.add(new ArrayList(li));
+            return result;
+            }
+        
+
+
+        if(target<0 || i>=n){
+            return result;
+        }
+
+        li.add(arr[i]);
+        function(arr,target-arr[i],n,i+1,li,result);
+        li.remove(li.size()-1);
+        while(i+1<n && arr[i]==arr[i+1]){
+            i++;
+        }
+
+        
+
+        function(arr,target,n,i+1,li,result);
+
+        return result;
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int n=candidates.length;
+        Arrays.sort(candidates);
+
+        List<List<Integer>> result=new ArrayList<>();
+        function(candidates,target,n,0,new ArrayList<>(),result);
+        return result;
+    }
+}
